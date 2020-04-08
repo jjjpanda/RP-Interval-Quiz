7824d5f4fe3c87ea17a525d177dd5866fd57e3e9
jjjpanda <15862136+jjjpanda@users.noreply.github.com>
2020-04-07 23:14:30 -0400

    updates

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -33,8 +33,6 @@ class Main extends React.Component{
                     <Content>
                         <Route exact path="/" render={(props) => (<Start {...props} />)} />
                         <Route exact path="/quiz" render={(props) => (<Quiz {...props} />)} />
-                        <Route exact path="/settings" render={(props) => (<Settings {...props} />)} />
-                        <Route exact path="/results" render={(props) => (<Results {...props} />)} />
                     </Content>
                 </Layout>
             </Router>
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
 
+import * as interval from '../lib/interval.js'
+
+import {
+    Button,
+    Spin
+} from 'antd'
+
 class Quiz extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            playing: false
+        }
+    }
+
+    playInterval = () => {
+        this.setState(() => ({ playing: true }), () => {
+            interval.playInterval(interval.randomInteger(150, 500), interval.findInterval(interval.randomInteger(1, 12) * (Math.random()>0.5 ? 1 : -1)).ratio, () => {
+                this.setState(() => ({ playing: false }))
+            })
+        })
+    }
+        
     render() {
         return (
-            <div>Quiz</div>
+            <div>
+                {this.state.playing ? <Spin /> : null}
+                <Button icon="play-cirle" onClick={this.playInterval} />
+            </div>
         )
     }
 }
