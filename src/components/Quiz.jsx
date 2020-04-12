63f6b05c6acd6d45e4bf372b677e1754db003827
jjjpanda <jawpanda13@gmail.com>
2020-04-11 22:21:36 -0400

    updates

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -4,30 +4,138 @@ import * as interval from '../lib/interval.js'
 
 import {
     Button,
-    Spin
+    Spin,
+    Progress
 } from 'antd'
+
+import Settings from './Settings.jsx'
 
 class Quiz extends React.Component{
     constructor(props){
         super(props)
+        interval.setProbability([
+            {semitones:-12, probability: 0 },
+            {semitones:-11, probability: 0 },
+            {semitones:-10, probability: 0 },
+            {semitones:-9, probability: 0 },
+            {semitones:-8, probability: 0 },
+            {semitones:-7, probability: 0 },
+            {semitones:-6, probability: 0 },
+            {semitones:-5, probability: 0 },
+            {semitones:-4, probability: 0 },
+            {semitones:-3, probability: 0 },
+            {semitones:-2, probability: 0 },
+            {semitones:-1, probability: 0 },
+            {semitones:1, probability: 0 },
+            {semitones:2, probability: 1/8 },
+            {semitones:3, probability: 0 },
+            {semitones:4, probability: 1/8 },
+            {semitones:5, probability: 1/8 },
+            {semitones:6, probability: 0 },
+            {semitones:7, probability: 1/8 },
+            {semitones:8, probability: 0 },
+            {semitones:9, probability: 1/8 },
+            {semitones:10, probability: 0 },
+            {semitones:11, probability: 1/8 },
+            {semitones:12, probability: 1/8 }
+        ])
         this.state = {
-            playing: false
+            playing: false,
+            firstPlay: true,
+            numberOfQuestions: 1,
+            numberCorrect: 0,
+            firstAttempt: true,
+            currentRoot: interval.randomInteger(150, 500),
+            currentInterval: interval.randomInterval(),
+            correctnessDistribution: interval.blankDistribution()
         }
+        console.log(this.state.correctnessDistribution)
     }
 
     playInterval = () => {
         this.setState(() => ({ playing: true }), () => {
-            interval.playInterval(interval.randomInteger(150, 500), interval.findInterval(interval.randomInteger(1, 12) * (Math.random()>0.5 ? 1 : -1)).ratio, () => {
-                this.setState(() => ({ playing: false }))
+            interval.playInterval(this.state.currentRoot, interval.findInterval(this.state.currentInterval).ratio, () => {
+                this.setState(() => ({ playing: false, firstPlay: false }))
             })
         })
+    }
+
+    sendAnswer = (semitones) => {
+        console.log(semitones)
+        if(semitones === this.state.currentInterval){
+            if(this.state.firstAttempt){
+                this.setState((state) => ({
+                    numberCorrect: state.numberCorrect+1, 
+                    numberOfQuestions: state.numberOfQuestions+1, 
+                    firstPlay: true,
+                    currentInterval: interval.randomInterval(),
+                    currentRoot: interval.randomInteger(150, 500)
+                }))
+            }
+            else{
+                this.setState((state) => ({ 
+                    numberOfQuestions: state.numberOfQuestions+1, 
+                    firstPlay: true,
+                    currentInterval: interval.randomInterval(),
+                    firstAttempt: true,
+                    currentRoot: interval.randomInteger(150, 500),
+                }))
+            }
+        }
+        else{
+            this.setState(() => ({ 
+                firstAttempt: false
+            }))
+        }
     }
         
     render() {
         return (
             <div>
                 {this.state.playing ? <Spin /> : null}
-                <Button icon="play-cirle" onClick={this.playInterval} />
+
+                <br />
+
+                <Settings updateSettings={() => {}} /> 
+
+                <span>
+                    <Button shape="round" icon={this.state.firstPlay ? "play-circle" : "redo"} onClick={this.playInterval} />
+                    <Button shape="round" icon="arrow-right" onClick={() => {}} />
+                </span>
+
+                <br />
+
+                <div>
+                    <Button shape="round" icon="up" onClick={() => {this.sendAnswer(12)}}>Octave</Button>
+                    <Button shape="round" icon="up" onClick={() => {this.sendAnswer(11)}}>Major 7th</Button>
+                    <Button shape="round" icon="up" onClick={() => {this.sendAnswer(10)}}>Minor 7th</Button>
+                    <Button shape="round" icon="up" onClick={() => {this.sendAnswer(9)}}>Major 6th</Button>
+                    <Button shape="round" icon="up" onClick={() => {this.sendAnswer(8)}}>Minor 6th</Button>
+                    <Button shape="round" icon="up" onClick={() => {this.sendAnswer(7)}}>Perfect Fifth</Button>
+                    <Button shape="round" icon="up" onClick={() => {this.sendAnswer(6)}}>Tritone</Button>
+                    <Button shape="round" icon="up" onClick={() => {this.sendAnswer(5)}}>Perfect Fourth</Button>
+                    <Button shape="round" icon="up" onClick={() => {this.sendAnswer(4)}}>Major Third</Button>
+                    <Button shape="round" icon="up" onClick={() => {this.sendAnswer(3)}}>Minor Third</Button>
+                    <Button shape="round" icon="up" onClick={() => {this.sendAnswer(2)}}>Major Second</Button>
+                    <Button shape="round" icon="up" onClick={() => {this.sendAnswer(1)}}>Minor Second</Button>
+                    
+                    <br />
+
+                    <Button shape="round" icon="down" onClick={() => {this.sendAnswer(-1)}}>Minor Second</Button>
+                    <Button shape="round" icon="down" onClick={() => {this.sendAnswer(-2)}}>Major Second</Button>
+                    <Button shape="round" icon="down" onClick={() => {this.sendAnswer(-3)}}>Minor Third</Button>
+                    <Button shape="round" icon="down" onClick={() => {this.sendAnswer(-4)}}>Major Third</Button>
+                    <Button shape="round" icon="down" onClick={() => {this.sendAnswer(-5)}}>Perfect Fourth</Button>
+                    <Button shape="round" icon="down" onClick={() => {this.sendAnswer(-6)}}>Tritone</Button>
+                    <Button shape="round" icon="down" onClick={() => {this.sendAnswer(-7)}}>Perfect Fifth</Button>
+                    <Button shape="round" icon="down" onClick={() => {this.sendAnswer(-8)}}>Minor 6th</Button>
+                    <Button shape="round" icon="down" onClick={() => {this.sendAnswer(-9)}}>Major 6th</Button>
+                    <Button shape="round" icon="down" onClick={() => {this.sendAnswer(-10)}}>Minor 7th</Button>
+                    <Button shape="round" icon="down" onClick={() => {this.sendAnswer(-11)}}>Major 7th</Button>
+                    <Button shape="round" icon="down" onClick={() => {this.sendAnswer(-12)}}>Octave</Button>
+                </div>
+
+                <Progress percent={this.state.numberCorrect/this.state.numberOfQuestions*100} showInfo={true} type="circle" format={p => `${Math.round(p*100)/100}% Correct`} />
             </div>
         )
     }
--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -1,9 +1,55 @@
 import React from 'react'
+import {
+    Button,
+    Modal, 
+    Radio,
+    Slider 
+} from 'antd'
 
 class Settings extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            value: 1,
+            visible: false,
+        };
+    }
+
+    onChange = e => {
+        console.log('radio checked', e.target.value);
+        this.setState({
+            value: e.target.value,
+        });
+    };
+
+    openModal = () => {
+        this.setState(() => ({visible: true}))
+    }
+
+    closeModal = () => {
+        this.setState(() => ({visible: false}), () => {
+            this.props.updateSettings(this.state)
+        })
+    }
+    
     render() {
         return (
-            <div>Settings</div>
+            <div>
+                <Button shape="round" icon="setting" onClick={this.openModal} />
+                <Modal
+                    visible={this.state.visible}
+                    footer={<Button onClick={this.closeModal} >Ok</Button>}
+                >
+                    <Radio.Group onChange={this.onChange} value={this.state.value}>
+                        <Radio value={1}>A</Radio>
+                        <Radio value={2}>B</Radio>
+                        <Radio value={3}>C</Radio>
+                    </Radio.Group>
+
+
+
+                </Modal>
+            </div>
         )
     }
 }
